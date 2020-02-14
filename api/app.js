c4d651b450a94e0ed82dd40905bd8cb4d645dbbf
Hardeep Singh <hardeep@cazisoft.com>
2020-02-14 01:20:48 -0800

    Introduce Docker

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const API_PORT = 1997;
-const DB_URL = "mongodb://localhost:27017/task-loco-api";
+const API_PORT = process.env.PORT || 8080;
+const DB_URL = "mongodb://db:27017/task-loco-api";
 
 mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(() => { console.log("Database Connected")})
