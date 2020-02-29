952da966491e79c6bc64332240a25a065f9106cc
Hardeep Singh <hardeep@cazisoft.com>
2020-02-29 07:46:06 -0800

    API Logic to Create Testing Data

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const API_PORT = process.env.PORT || 8080;
 const DB_URL = "mongodb://localhost:27017/task-loco-api";
+const testData = require('./test/TestData');
 
 mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(() => { console.log("Database Connected")})
@@ -14,3 +15,8 @@ app.get("/", (req, res) => {res.json({api: "Welcome to Task Loco Api"})});
 app.use('/user', require('./route/User'));
 app.use('/task', require('./route/Task'));
 app.listen(API_PORT, function () {console.log("Task Loco API is running on port: " + API_PORT)});
+
+//Generate Test Task Data
+// testData.createPendingTasks(10);
+// testData.createInProgressTasks(10);
+// testData.createCompletedTasks(10);
---
+++ api/test/TestData.js
@@ -0,0 +1,56 @@
+const Task = require('../model/Task');
+
+//Create Pending Tasks (even High Priority, odd Standard Priority)
+exports.createPendingTasks = (startIndex) => {
+    for (var i = startIndex; i < startIndex + 10; i++) {
+        new Task({
+            title: "Task " + i,
+            description: "Task Description " + i,
+            completeBy: new Date().addDays(i),
+            assignee: "Hardeep " + i,
+            responsible: i % 2 === 0 ? "Sona " + i : "",
+            priority: i % 2 === 0 ? "High" : "Standard",
+            status: "Pending"
+        }).save()
+    }
+    console.log("Generated Pending Tasks")
+};
+
+//Create In-Progress Tasks (even High Priority, odd Standard Priority)
+exports.createInProgressTasks = (startIndex) => {
+    for (var i = startIndex; i < startIndex + 10; i++) {
+        new Task({
+            title: "Tweet " + i,
+            description: "Tweet Description " + i,
+            completeBy: new Date().addDays(i),
+            assignee: "Sona " + i,
+            responsible: i % 2 === 0 ? "Sahindeep " + i : "",
+            priority: i % 2 === 0 ? "High" : "Standard",
+            status: "In Progress"
+        }).save()
+    }
+    console.log("Generated InProgress Tasks")
+};
+
+//Create Completed Tasks (even High Priority, odd Standard Priority)
+exports.createCompletedTasks = (startIndex) => {
+    for (var i = startIndex; i < startIndex + 10; i++) {
+        new Task({
+            title: "Story " + i,
+            description: "Story Description " + i,
+            completeBy: new Date().addDays(i),
+            assignee: "Sahibdeep " + i,
+            responsible: i % 2 === 0 ? "Sona " + i : "",
+            priority: i % 2 === 0 ? "High" : "Standard",
+            status: "Completed"
+        }).save();
+    }
+    console.log("Generated Completed Tasks")
+};
+
+
+// Get Date with Days
+Date.prototype.addDays = function(days) {
+    this.setDate(this.getDate() + parseInt(days));
+    return this;
+};
